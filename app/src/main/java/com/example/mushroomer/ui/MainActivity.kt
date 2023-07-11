1dff889603233d1070e012258ce7a58d6d70c369
nbmlon99 <rhfwleowkd77@naver.com>
2023-07-11 21:08:00 +0900

    <Feature> 바텀 네비게이션 구현 (MainActivity)

--- app/src/main/java/com/example/mushroomer/ui/MainActivity.kt
+++ app/src/main/java/com/example/mushroomer/ui/MainActivity.kt
@@ -1,46 +1,63 @@
-package com.example.mushroomer.ui
-
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.mushroomer.ui.theme.MushroomerTheme
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mushroomer.R
+import com.example.mushroomer.databinding.ActivityMainBinding
+import com.example.mushroomer.ui.community.CommunityFragment
+import com.example.mushroomer.ui.dogam.DogamFragment
+import com.example.mushroomer.ui.map.MapFragment
+import com.example.mushroomer.ui.profile.ProfileFragment
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding // 뷰 바인딩 변수
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MushroomerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+        binding = ActivityMainBinding.inflate(layoutInflater) // 뷰 바인딩 초기화
+        setContentView(binding.root) // setContentView에 뷰 바인딩의 루트 요소를 전달
+
+        // 뷰 바인딩을 통해 바텀 네비게이션 뷰 참조
+        val bottomNavigationView = binding.bottomNavigationView
+
+        bottomNavigationView.setOnItemSelectedListener  { menuItem ->
+            when (menuItem.itemId) {
+                R.id.dogam -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.FragmentContainer, DogamFragment())
+                        .commit()
+                    true
                 }
+                R.id.map -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.FragmentContainer, MapFragment())
+                        .commit()
+                    true
+                }
+                R.id.camera -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.FragmentContainer, CameraFragment())
+                        .commit()
+                    true
+                }
+
+                R.id.community -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.FragmentContainer, CommunityFragment())
+                        .commit()
+                    true
+                }
+                R.id.profile -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.FragmentContainer, ProfileFragment())
+                        .commit()
+                    true
+                }
+                else -> false
             }
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MushroomerTheme {
-        Greeting("Android")
+        // 초기 프래그먼트를 설정
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.FragmentContainer, CameraFragment())
+            .commit()
     }
 }
