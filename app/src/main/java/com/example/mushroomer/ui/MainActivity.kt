39d94571eff880c4933e023585032d3d87e60519
nbmlon99 <rhfwleowkd77@gmail.com>
2023-07-15 23:19:14 +0900

    <Chore> Community -> Commu 이름 변경

--- app/src/main/java/com/example/mushroomer/ui/MainActivity.kt
+++ app/src/main/java/com/example/mushroomer/ui/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.mushroomer.R
 import com.example.mushroomer.databinding.ActivityMainBinding
 import com.example.mushroomer.ui.camera.CameraFragment
-import com.example.mushroomer.ui.community.CommunityFragment
+import com.example.mushroomer.ui.commu.CommuFragment
 import com.example.mushroomer.ui.dogam.DogamFragment
 import com.example.mushroomer.ui.map.MapFragment
 import com.example.mushroomer.ui.profile.ProfileFragment
@@ -45,7 +45,7 @@ class MainActivity : AppCompatActivity() {
 
                 R.id.community -> {
                     supportFragmentManager.beginTransaction()
-                        .replace(R.id.FragmentContainer, CommunityFragment())
+                        .replace(R.id.FragmentContainer, CommuFragment())
                         .commit()
                     true
                 }
