8dc933d17380e53182976d5e68c0757d39b08a52
nbmlon99 <rhfwleowkd77@gmail.com>
2023-07-15 23:16:47 +0900

    <Chore> Community -> Commu 이름 변경

--- app/src/main/java/com/example/mushroomer/ui/commu/CommuFragment.kt
+++ app/src/main/java/com/example/mushroomer/ui/commu/CommuFragment.kt
@@ -1,4 +1,4 @@
-package com.example.mushroomer.ui.community
+package com.example.mushroomer.ui.commu
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [CommunityFragment.newInstance] factory method to
+ * Use the [CommuFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class CommunityFragment : Fragment() {
+class CommuFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class CommunityFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_community, container, false)
+        return inflater.inflate(R.layout.fragment_commu, container, false)
     }
 
     companion object {
@@ -50,7 +50,7 @@ class CommunityFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            CommunityFragment().apply {
+            CommuFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
