9810d44f8488f7941cb5bb5b9949ced20b2a0c08
rhfwleowkd77@gmail.com <rhfwleowkd77@gmail.com>
2023-07-27 03:05:08 +0900

    <Chore> #1 Fragment Renaming

--- app/src/main/java/com/example/mushroomer/ui/dogam/DogamFragment_detail.kt
+++ app/src/main/java/com/example/mushroomer/ui/dogam/DogamFragment_detail.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [DogamItemFragment.newInstance] factory method to
+ * Use the [DogamFragment_detail.newInstance] factory method to
  * create an instance of this fragment.
  */
-class DogamItemFragment : Fragment() {
+class DogamFragment_detail : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class DogamItemFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dogam_item, container, false)
+        return inflater.inflate(R.layout.fragment_dogam_detail, container, false)
     }
 
     companion object {
@@ -50,7 +50,7 @@ class DogamItemFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            DogamItemFragment().apply {
+            DogamFragment_detail().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
