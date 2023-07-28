4d8f38afb4d9f7366225cbff2196b8025ed81b1c
rhfwleowkd77@gmail.com <rhfwleowkd77@gmail.com>
2023-07-28 15:57:45 +0900

    <Chore> 리터럴 상수 정의

---
+++ app/src/main/java/com/example/mushroomer/MyApplication.kt
@@ -0,0 +1,11 @@
+package com.example.mushroomer
+
+import android.app.Application
+
+class MyApplication : Application() {
+    class Constants {
+        companion object {
+            const val UNDEFINED = -1
+        }
+    }
+}
