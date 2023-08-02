655888479c090d0ad5bca24c8966cdae946cc290
nbmLon99 <rhfwleowkd77@gmail.com>
2023-08-02 11:24:51 +0900

    <Feature> CameraX 활용한 카메라 프래그먼트 전면 수정

--- app/src/main/java/com/nbmlon/mushroomer/ui/camera/CameraFragment.kt
+++ app/src/main/java/com/nbmlon/mushroomer/ui/camera/CameraFragment.kt
@@ -1,7 +1,15 @@
 package com.nbmlon.mushroomer.ui.camera
 
 import android.Manifest
+import android.app.AlertDialog
+import android.content.ContentResolver
+import android.content.ContentValues
+import android.content.DialogInterface
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.ImageFormat
+import android.hardware.Camera
 import android.hardware.camera2.CameraAccessException
 import android.hardware.camera2.CameraCaptureSession
 import android.hardware.camera2.CameraDevice
@@ -11,30 +19,62 @@ import android.hardware.camera2.CaptureResult
 import android.hardware.camera2.TotalCaptureResult
 import android.hardware.camera2.params.OutputConfiguration
 import android.hardware.camera2.params.SessionConfiguration
+import android.media.Image
+import android.media.ImageReader
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import com.nbmlon.mushroomer.databinding.FragmentCameraBinding
+import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
+import java.nio.ByteBuffer
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
-class CameraFragment : Fragment() {
-    private lateinit var binding: FragmentCameraBinding
-    private lateinit var picturesAdapter : PicturesAdapter
-    private lateinit var cameraManager: CameraManager
-    private lateinit var cameraDevice: CameraDevice
-    private lateinit var captureSession: CameraCaptureSession
-
-
-    private val cameraId: String by lazy {
-        cameraManager.cameraIdList[0]
+/** Helper type alias used for analysis use case callbacks */
+typealias LumaListener = (luma: Double) -> Unit
+class CameraFragment : Fragment(), ImageManager {
+    companion object {
+        private const val TAG = "CameraFragment"
+        private const val FILENAME_FORMAT = "yyyyMMdd_HHmmss"
+        private const val CAMERA_PERMISSION_REQUEST_CODE = 100
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf (
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            }.toTypedArray()
     }
+    private lateinit var binding: FragmentCameraBinding
+    private lateinit var picturesAdapter: PicturesAdapter
+    private lateinit var cameraExecutor: ExecutorService
+
+    private var imageCapture: ImageCapture? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -47,144 +87,157 @@ class CameraFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        cameraManager = requireActivity().getSystemService(CameraManager::class.java)
-        picturesAdapter = PicturesAdapter()
+        picturesAdapter = PicturesAdapter(this@CameraFragment as ImageManager)
         binding.pictureRV.adapter = picturesAdapter
-        binding.shootBtn.setOnClickListener {
-            takePhoto()
-        }
+        binding.shootBtn.setOnClickListener { takePhoto() }
+        cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
     override fun onResume() {
         super.onResume()
-        if (ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.CAMERA
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                requireActivity(),
-                arrayOf(Manifest.permission.CAMERA),
-                CAMERA_PERMISSION_REQUEST_CODE
-            )
+
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
         } else {
-            openCamera()
+            ActivityCompat.requestPermissions(
+                requireActivity(), REQUIRED_PERMISSIONS, CAMERA_PERMISSION_REQUEST_CODE)
         }
     }
 
     override fun onPause() {
         super.onPause()
-        closeCamera()
+        cameraExecutor.shutdown()
     }
 
-    private fun openCamera() {
-        try {
-            if (ActivityCompat.checkSelfPermission(
-                    requireContext(),
-                    Manifest.permission.CAMERA
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                ActivityCompat.requestPermissions(
-                    requireActivity(),
-                    arrayOf(Manifest.permission.CAMERA),
-                    CAMERA_PERMISSION_REQUEST_CODE
-                )
+    private fun startCamera() {
+        //ProcessCameraProvider 인스턴스를 만듭니다. 이는 카메라의 수명 주기를 수명 주기 소유자와 바인딩하는 데 사용합니다. CameraX가 수명 주기를 인식하므로 카메라를 열고 닫는 작업이 필요하지 않게 됩니다.
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.cameraPreview.surfaceProvider)
+                }
+
+            imageCapture = ImageCapture.Builder()
+                .build()
+
+//            val imageAnalyzer = ImageAnalysis.Builder()
+//                .build()
+//                .also {
+//                    it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
+//                        Log.d(TAG, "Average luminosity: $luma")
+//                    })
+//                }
+
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture)
+
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
             }
-            cameraManager.openCamera(cameraId, object : CameraDevice.StateCallback() {
-                override fun onOpened(camera: CameraDevice) {
-                    cameraDevice = camera
-                    createCaptureSession()
-                }
 
-                override fun onDisconnected(camera: CameraDevice) {
-                    camera.close()
-                }
-
-                override fun onError(camera: CameraDevice, error: Int) {
-                    camera.close()
-                }
-            }, null)
-        } catch (e: CameraAccessException) {
-            e.printStackTrace()
-        }
-    }
-
-    private fun closeCamera() {
-        captureSession.close()
-        cameraDevice.close()
-    }
-
-    private fun createCaptureSession() {
-        val surfaceHolder = binding.cameraPreview.holder
-        val captureRequestBuilder =
-            cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
-        captureRequestBuilder.addTarget(surfaceHolder.surface)
-
-        val sessionConfiguration = SessionConfiguration(
-            SessionConfiguration.SESSION_REGULAR,
-            listOf(OutputConfiguration(surfaceHolder.surface)),
-            requireActivity().mainExecutor,
-            object : CameraCaptureSession.StateCallback() {
-                override fun onConfigured(session: CameraCaptureSession) {
-                    captureSession = session
-                    captureSession.setRepeatingRequest(
-                        captureRequestBuilder.build(),
-                        null,
-                        null
-                    )
-                }
-
-                override fun onConfigureFailed(session: CameraCaptureSession) {
-                    session.close()
-                }
-            }
-        )
-
-        try {
-            cameraDevice.createCaptureSession(sessionConfiguration)
-        } catch (e: CameraAccessException) {
-            e.printStackTrace()
-        }
+        }, ContextCompat.getMainExecutor(requireContext()))
     }
 
     private fun takePhoto() {
-        val surfaceHolder  = binding.cameraPreview.holder
-        val captureRequestBuilder =
-            cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE)
-        captureRequestBuilder.addTarget(surfaceHolder.surface)
+        Log.d(TAG,"촬영됨")
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture =  imageCapture ?: return
 
-        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
-        val fileName = "IMG_$timeStamp.jpg"
-        val filePath =
-            requireActivity().getExternalFilesDir(null)?.absolutePath + "/" + fileName
-        val fileOutputStream = FileOutputStream(filePath)
+        // Create time stamped name and MediaStore entry.
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+            .format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
+            }
+        }
 
-        captureSession.capture(
-            captureRequestBuilder.build(),
-            object : CameraCaptureSession.CaptureCallback() {
-                override fun onCaptureCompleted(
-                    session: CameraCaptureSession,
-                    request: CaptureRequest,
-                    result: TotalCaptureResult
-                ) {
-                    try {
-                        result.get(CaptureResult.JPEG_ORIENTATION)
-                            ?.let { fileOutputStream.write(it) }
-                        fileOutputStream.close()
-                        picturesAdapter.addPicture(filePath)
-                    } catch (e: IOException) {
-                        e.printStackTrace()
-                    }
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions
+            .Builder(
+                requireActivity().contentResolver,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues)
+            .build()
 
-                    // 이미지 저장 후 카메라 미리 보기 재시작
-                    createCaptureSession()
+        // Set up image capture listener, which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(requireContext()),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
                 }
-            },
-            null
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults){
+
+                    val msg = "Photo capture succeeded: ${output.savedUri}"
+                    picturesAdapter.addPicture(output.savedUri!!)
+                    Log.d(TAG, msg)
+                }
+            }
         )
     }
 
-    companion object {
-        private const val CAMERA_PERMISSION_REQUEST_CODE = 100
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            requireContext(), it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private class LuminosityAnalyzer(private val listener: LumaListener) : ImageAnalysis.Analyzer {
+
+        private fun ByteBuffer.toByteArray(): ByteArray {
+            rewind()    // Rewind the buffer to zero
+            val data = ByteArray(remaining())
+            get(data)   // Copy the buffer into a byte array
+            return data // Return the byte array
+        }
+
+        override fun analyze(image: ImageProxy) {
+
+            val buffer = image.planes[0].buffer
+            val data = buffer.toByteArray()
+            val pixels = data.map { it.toInt() and 0xFF }
+            val luma = pixels.average()
+
+            listener(luma)
+
+            image.close()
+        }
+    }
+
+
+    override fun deleteImage(uri: Uri) {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+
+            requireActivity().contentResolver.delete(uri, null, null)
+        }else{
+            Toast.makeText(requireActivity(),"권한이없음",Toast.LENGTH_LONG).show()
+        }
+
     }
 }
---
+++ app/src/main/java/com/nbmlon/mushroomer/ui/camera/ImageManager.kt
@@ -0,0 +1,7 @@
+package com.nbmlon.mushroomer.ui.camera
+
+import android.net.Uri
+
+interface ImageManager {
+    fun deleteImage(uri : Uri)
+}
