e88a0e91f5cfeaf4c1647243aec4205fe35eddc3
nbmLon99 <rhfwleowkd77@gmail.com>
2023-08-02 11:25:20 +0900

    <WontFix> 사진 삭제하는거 index 에러 문제 해결필요

--- app/src/main/java/com/nbmlon/mushroomer/ui/camera/PicturesAdapter.kt
+++ app/src/main/java/com/nbmlon/mushroomer/ui/camera/PicturesAdapter.kt
@@ -1,29 +1,39 @@
 package com.nbmlon.mushroomer.ui.camera
 
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.nbmlon.mushroomer.GlideApp
 import com.nbmlon.mushroomer.databinding.ItemPhotoCheckingBinding
 import com.nbmlon.mushroomer.ui.camera.PicturesAdapter.PictureViewHolder
 import java.io.File
+import javax.sql.DataSource
 
 
-class PicturesAdapter() : RecyclerView.Adapter<PictureViewHolder>() {
+class PicturesAdapter(im: ImageManager) : RecyclerView.Adapter<PictureViewHolder>() {
     private lateinit var itemBinding: ItemPhotoCheckingBinding
-    private val pictures : ArrayList<String> = ArrayList()
+    private var imageManager: ImageManager = im
+    private val pictures : ArrayList<Uri> = ArrayList()
 
 
     inner class PictureViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
         fun bind(pos : Int){
             GlideApp.with(itemView.context)
                 .load(pictures[pos])
-                .into(itemBinding.photo)
+                    .into(itemBinding.photo)
 
             itemBinding.clearBtn.setOnClickListener {
-                removePicture(pos)
+                imageManager.deleteImage(pictures[pos])
+                pictures.removeAt(absoluteAdapterPosition)
+                notifyItemRemoved(absoluteAdapterPosition)
             }
         }
     }
@@ -42,20 +52,16 @@ class PicturesAdapter() : RecyclerView.Adapter<PictureViewHolder>() {
     }
 
 
-    private fun removePicture(pos : Int){
-        //사진 삭제
-        val photoFile: File = File(pictures[pos])
-        if (photoFile.exists()) {
-            photoFile.delete()
-        } else {
-            Log.d("에러","사진 반환 실패")
-        }
-        pictures.removeAt(pos)
-        notifyItemRemoved(pos)
+
+    //wontfix
+    private fun removePicture(uri : Uri){
+        val acPos = pictures.indexOf(uri)
+        pictures.removeAt(acPos)
+        notifyItemRemoved(acPos)
     }
 
-    fun addPicture(path : String){
-        pictures.add(path)
+    fun addPicture(uri : Uri){
+        pictures.add(uri)
         notifyItemInserted(pictures.size-1)
     }
 }
