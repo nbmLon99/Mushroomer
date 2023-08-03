52f205e90d873e5db237c4b5f347791274caad96
nbmLon99 <rhfwleowkd77@gmail.com>
2023-08-03 09:39:13 +0900

    <Fix> #3 RecyclerView ItemViewBinding 관련 문제 해결

--- app/src/main/java/com/nbmlon/mushroomer/ui/camera/PicturesAdapter.kt
+++ app/src/main/java/com/nbmlon/mushroomer/ui/camera/PicturesAdapter.kt
@@ -1,9 +1,13 @@
 package com.nbmlon.mushroomer.ui.camera
 
 import android.net.Uri
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.nbmlon.mushroomer.GlideApp
 import com.nbmlon.mushroomer.databinding.ItemPhotoCheckingBinding
@@ -15,26 +19,22 @@ class PicturesAdapter(im: ImageListner) : RecyclerView.Adapter<PictureViewHolder
     private var imageManager: ImageListner = im
     private val pictures : ArrayList<Uri> = ArrayList()
 
-
-    inner class PictureViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+    inner class PictureViewHolder(private val itemBinding: ItemPhotoCheckingBinding) : RecyclerView.ViewHolder(itemBinding.root){
         fun bind(pos : Int){
+            val targetUri = pictures[pos]
             GlideApp.with(itemView.context)
-                .load(pictures[pos])
+                .load(targetUri)
                     .into(itemBinding.photo)
-
             itemBinding.clearBtn.setOnClickListener {
-                imageManager.deleteImage(pictures[pos])
-                pictures.removeAt(pos)
-                notifyItemRemoved(pos)
-                // 포스팅!! -> 왜 필요할까??
-                notifyItemRangeChanged(pos,itemCount)
+                imageManager.deleteImage(targetUri)
+                removeItem(targetUri)
             }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PictureViewHolder {
         itemBinding = ItemPhotoCheckingBinding.inflate(LayoutInflater.from(parent.context))
-        return PictureViewHolder(itemBinding.root)
+        return PictureViewHolder(itemBinding)
     }
 
     override fun getItemCount(): Int {
@@ -45,9 +45,17 @@ class PicturesAdapter(im: ImageListner) : RecyclerView.Adapter<PictureViewHolder
         holder.bind(position)
     }
 
+
+    private fun removeItem(targetUri :Uri){
+        val pos = pictures.indexOf(targetUri)
+        pictures.removeAt(pos)
+        notifyItemRemoved(pos)
+        notifyItemRangeChanged(pos,itemCount)
+    }
+
     fun addPicture(uri : Uri){
-        pictures.add(uri)
-        notifyItemInserted(pictures.size-1)
+        pictures.add(0,uri)
+        notifyItemInserted(0)
     }
 
     fun getPictures() : ArrayList<Uri>{
