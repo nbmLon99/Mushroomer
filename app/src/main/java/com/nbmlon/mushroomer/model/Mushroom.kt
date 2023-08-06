7ed1dbaa4eb7bcc6a7e0e08f8786c89302a53ebb
nbmLon99 <rhfwleowkd77@gmail.com>
2023-08-06 13:39:48 +0900

    <Feature> #5 Data Binding 적용
    + DogamBindingAdapter.kt 설정으로 imageUrl, 포획여부 구분
    + item_dogam.xml에서 databinding 사용하게 변경
    + Mushroom 데이터 모델 약간 수정

--- app/src/main/java/com/nbmlon/mushroomer/model/Mushroom.kt
+++ app/src/main/java/com/nbmlon/mushroomer/model/Mushroom.kt
@@ -1,5 +1,7 @@
 package com.nbmlon.mushroomer.model
 
+import retrofit2.http.Url
+
 enum class MushType{
     EDIBLE,
     POISON
@@ -8,17 +10,21 @@ enum class MushType{
 
 /**
  * @param dogamNo   도감 넘버
+ * @param imageUrl  사진 url
  * @param name      버섯 이름
  * @param feature   특징
  * @param type      버섯 타입 ( 독버섯, 식용버섯 )
  * @param rarity    희귀도
- * @param myPicPath 나의 발견 사진 path (path에 날짜 저장 예정)
+ * @param myPicPath 나의 발견 사진 path (path에 날짜 저장 예정) -> 0개일 시 발견 못한 버섯으로 취급
  */
 data class Mushroom (
         val dogamNo : Int,
+        val imageUrl : String,
         val name : String,
         val feature : String,
         val type : MushType,
         val rarity : Long,
         val myPicPath : ArrayList<String>
-        )
+        ){
+    val gotcha : Boolean get() =  myPicPath.size > 0
+}
---
+++ app/src/main/java/com/nbmlon/mushroomer/ui/dogam/DogamBindingAdapter.kt
@@ -0,0 +1,27 @@
+package com.nbmlon.mushroomer.ui.dogam
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+
+@BindingAdapter("imageFromUrl")
+fun bindImageFromUrl(view: ImageView, imageUrl: String?) {
+    if (!imageUrl.isNullOrEmpty()) {
+        Glide.with(view.context)
+            .load(imageUrl)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .into(view)
+    }
+}
+
+@BindingAdapter("isDiscovered")
+fun bindIsGone(view: FloatingActionButton, gotcha: Boolean) {
+    if (gotcha) {
+        view.hide()
+    } else {
+        view.show()
+    }
+}
