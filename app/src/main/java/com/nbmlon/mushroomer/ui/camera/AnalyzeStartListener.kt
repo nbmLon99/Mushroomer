f2c4acd073218b26f8c4f258b3584c7e724262a7
nbmLon99 <rhfwleowkd77@gmail.com>
2023-08-17 12:21:41 +0900

    <Feature> 검사시작 버튼 클릭시 로딩바 구현

--- app/src/main/java/com/nbmlon/mushroomer/ui/camera/AnalyzeStartListener.kt
+++ app/src/main/java/com/nbmlon/mushroomer/ui/camera/AnalyzeStartListener.kt
@@ -1,7 +1,9 @@
 package com.nbmlon.mushroomer.ui.camera
 
+import java.io.Serializable
+
 
 /** CameraFragment_alert 다이얼로그에서 검사 진행을 클릭 **/
-interface AnalyzeStartListener {
+fun interface AnalyzeStartListener : Serializable {
     fun startAnalyze()
 }
--- app/src/main/java/com/nbmlon/mushroomer/ui/camera/CameraFragment_alert.kt
+++ app/src/main/java/com/nbmlon/mushroomer/ui/camera/CameraFragment_alert.kt
@@ -3,6 +3,7 @@ package com.nbmlon.mushroomer.ui.camera
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 import com.nbmlon.mushroomer.databinding.FragmentCameraAlertBinding
@@ -12,9 +13,11 @@ class CameraFragment_alert : DialogFragment() {
     companion object {
         const val TAG = "CameraFragment_alert"
         const val ITEM_COUNT = "itemCount"
+        const val START_ANALYZE_LISTENER = "AnalyzeStartListener"
     }
 
     private var itemCount = -1
+    private lateinit var startListener : AnalyzeStartListener
     private lateinit var binding : FragmentCameraAlertBinding
 
 
@@ -22,6 +25,11 @@ class CameraFragment_alert : DialogFragment() {
         super.onCreate(savedInstanceState)
         arguments?.let {
             itemCount = it.getInt(ITEM_COUNT, -1)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                startListener = it.getSerializable(START_ANALYZE_LISTENER, AnalyzeStartListener::class.java)!!
+            }else{
+                startListener = it.getSerializable(START_ANALYZE_LISTENER) as AnalyzeStartListener
+            }
         }
     }
 
@@ -30,7 +38,10 @@ class CameraFragment_alert : DialogFragment() {
         binding.apply {
             picSize = itemCount
             btnCancel.setOnClickListener { this@CameraFragment_alert.dismiss() }
-            btnAnalyze.setOnClickListener { (parentFragment as AnalyzeStartListener).startAnalyze() }
+            btnAnalyze.setOnClickListener {
+                startListener.startAnalyze()
+                this@CameraFragment_alert.dismiss()
+            }
             val builder = AlertDialog.Builder(requireActivity())
             builder.setView(root)
             return builder.create()
