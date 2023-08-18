dbb02f491589e5fe8fb5e5b6865e5e1e2f970813
nbmLon99 <rhfwleowkd77@gmail.com>
2023-08-19 06:19:46 +0900

    <Feature> #13 지문 인증 등록 임시함수 구현 ( 설정화면 )

--- app/src/main/java/com/nbmlon/mushroomer/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/nbmlon/mushroomer/ui/profile/ProfileFragment.kt
@@ -1,11 +1,21 @@
 package com.nbmlon.mushroomer.ui.profile
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import com.nbmlon.mushroomer.AppUser
 import com.nbmlon.mushroomer.R
+import com.nbmlon.mushroomer.ui.login.BiometricPromptUtils
+import com.nbmlon.mushroomer.ui.login.CIPHERTEXT_WRAPPER
+import com.nbmlon.mushroomer.ui.login.SHARED_PREFS_FILENAME
+import com.nbmlon.mushroomer.utils.CryptographyManager
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +31,8 @@ class ProfileFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    val cryptographyManager = CryptographyManager()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,5 +68,43 @@ class ProfileFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+
+
+
+
+
+
+
+
+
+    fun showBiometricPromptForEncryption() {
+        val canAuthenticate = BiometricManager.from(requireActivity().applicationContext).canAuthenticate()
+        if (canAuthenticate == BiometricManager.BIOMETRIC_SUCCESS) {
+            val secretKeyName = "biometric_sample_encryption_key"
+            val cipher = cryptographyManager.getInitializedCipherForEncryption(secretKeyName)
+            val biometricPrompt =
+                BiometricPromptUtils.createBiometricPrompt(requireActivity()as AppCompatActivity, ::encryptAndStoreServerToken)
+            val promptInfo = BiometricPromptUtils.createPromptInfo(requireActivity() as AppCompatActivity)
+            biometricPrompt.authenticate(promptInfo, BiometricPrompt.CryptoObject(cipher))
+        }
+    }
+
+    private fun encryptAndStoreServerToken(authResult: BiometricPrompt.AuthenticationResult) {
+        authResult.cryptoObject?.cipher?.apply {
+            AppUser.token?.let { token ->
+                //Log.d(TAG, "The token from server is $token")
+                val encryptedServerTokenWrapper = cryptographyManager.encryptData(token, this)
+                cryptographyManager.persistCiphertextWrapperToSharedPrefs(
+                    encryptedServerTokenWrapper,
+                    requireActivity().applicationContext,
+                    SHARED_PREFS_FILENAME,
+                    Context.MODE_PRIVATE,
+                    CIPHERTEXT_WRAPPER
+                )
+            }
+        }
+        //finish()
     }
 }
