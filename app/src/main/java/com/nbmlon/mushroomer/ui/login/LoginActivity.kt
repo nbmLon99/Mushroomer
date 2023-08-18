8bbc547bf64c24e2b5767b1c3e82a61b6ca8597f
nbmLon99 <rhfwleowkd77@gmail.com>
2023-08-18 16:36:45 +0900

    <Feature> #13 로그인 뷰 모델 정의

--- app/src/main/java/com/nbmlon/mushroomer/ui/login/LoginActivity.kt
+++ app/src/main/java/com/nbmlon/mushroomer/ui/login/LoginActivity.kt
@@ -1,12 +1,89 @@
 package com.nbmlon.mushroomer.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.nbmlon.mushroomer.R
+import android.util.Log
+import android.view.inputmethod.EditorInfo
+import androidx.activity.viewModels
+import androidx.core.widget.doAfterTextChanged
+import androidx.lifecycle.Observer
+import com.nbmlon.mushroomer.AppUser
+import com.nbmlon.mushroomer.databinding.ActivityLoginBinding
+import com.nbmlon.mushroomer.ui.MainActivity
 
 class LoginActivity : AppCompatActivity() {
+   companion object{
+       const val TAG = "LoginActivity"
+   }
+
+    private lateinit var binding: ActivityLoginBinding
+    private val loginViewModel by viewModels<LoginViewModel>()
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupForLoginWithPassword()
+    }
+
+
+
+    private fun setupForLoginWithPassword() {
+        loginViewModel.loginWithPasswordFormState.observe(this, Observer { formState ->
+            val loginState = formState ?: return@Observer
+            when (loginState) {
+                is SuccessfulLoginFormState -> binding.login.isEnabled = loginState.isDataValid
+                is FailedLoginFormState -> {
+                    loginState.usernameError?.let { binding.username.error = getString(it) }
+                    loginState.passwordError?.let { binding.password.error = getString(it) }
+                }
+            }
+        })
+        loginViewModel.loginResult.observe(this, Observer {
+            val loginResult = it ?: return@Observer
+            if (loginResult.success) {
+                Log.d(TAG,
+                    "You successfully signed up using password as: user " +
+                            "${AppUser.user?.name} with fake token ${AppUser.token}")
+                updateApp()
+            }
+        })
+        binding.username.doAfterTextChanged {
+            loginViewModel.onLoginDataChanged(
+                binding.username.text.toString(),
+                binding.password.text.toString()
+            )
+        }
+        binding.password.doAfterTextChanged {
+            loginViewModel.onLoginDataChanged(
+                binding.username.text.toString(),
+                binding.password.text.toString()
+            )
+        }
+//        binding.password.setOnEditorActionListener { _, actionId, _ ->
+//            when (actionId) {
+//                EditorInfo.IME_ACTION_DONE ->
+//                    loginViewModel.login(
+//                        binding.username.text.toString(),
+//                        binding.password.text.toString()
+//                    )
+//            }
+//            false
+//        }
+        binding.login.setOnClickListener {
+            loginViewModel.login(
+                binding.username.text.toString(),
+                binding.password.text.toString()
+            )
+            Log.d(TAG, "Username ${AppUser.user?.name}; fake token ${AppUser.token}")
+        }
+        Log.d(TAG, "Username ${AppUser.user?.name}; fake token ${AppUser.token}")
+    }
+
+    private fun updateApp() {
+        startActivity(Intent(this,MainActivity::class.java))
     }
 }
---
+++ app/src/main/java/com/nbmlon/mushroomer/ui/login/LoginResult.kt
@@ -0,0 +1,2 @@
+package com.nbmlon.mushroomer.ui.login
+data class LoginResult(val success: Boolean = false)
