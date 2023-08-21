ac914134db571325f967509b86beef87d4c2fc38
nbmLon99 <rhfwleowkd77@gmail.com>
2023-08-21 19:30:51 +0900

    <Refactor> #7 답글 표시 방식 수정 -> isReply 삭제

--- app/src/main/java/com/nbmlon/mushroomer/model/Comment.kt
+++ app/src/main/java/com/nbmlon/mushroomer/model/Comment.kt
@@ -16,14 +16,12 @@ import java.util.Date
  * @param time          작성시간
  * @param writer        작성자
  * @param replies       답글 리스트 ( 코멘트 )
- * @param isReply       답글?
  */
 data class Comment(
     val writer : User,
     val content : String,
     val time : DateTime,
     val replies : ArrayList<Comment>?,
-    val isReply : Boolean
 ){
     val isMine = writer == AppUser.user
 }
