367170cbbb0c74b87ae4069f126832da43b65236
nbmLon99 <rhfwleowkd77@gmail.com>
2023-09-02 16:39:26 +0900

    <Feature> #5 사용자에게 사진기록 보여주는 다이얼로그 구현
    - goAnother(맵 / 도감 상호이동 구현)
    - goPicBoard(사진 자랑 게시판으로 이동

--- app/src/main/java/com/nbmlon/mushroomer/ui/map/MapFragment.kt
+++ app/src/main/java/com/nbmlon/mushroomer/ui/map/MapFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.nbmlon.mushroomer.R
+import com.nbmlon.mushroomer.model.MushHistory
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,14 +14,23 @@ private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 /**
- * A simple [Fragment] subclass.
- * Use the [MapFragment.newInstance] factory method to
- * create an instance of this fragment.
+ *
  */
 class MapFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    companion object {
+        const val TAG = "MapFragment"
+
+        @JvmStatic
+        fun getFocusedFor(mushHistory: MushHistory) =
+            MapFragment().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,14 +48,5 @@ class MapFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_map, container, false)
     }
 
-    companion object {
-        const val TAG = "MapFragment"
 
-        @JvmStatic
-        fun newInstance() =
-            MapFragment().apply {
-                arguments = Bundle().apply {
-                }
-            }
-    }
 }
