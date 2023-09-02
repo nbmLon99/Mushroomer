22df4199d5a1275dcba446a01469bee739ad813f
nbmLon99 <rhfwleowkd77@gmail.com>
2023-09-02 16:29:41 +0900

    <Chore> BindingAdapter static으로 해결했던 문제 -> namespace 지정 때문
    - static 제거

--- app/src/main/java/com/nbmlon/mushroomer/model/MushHistory.kt
+++ app/src/main/java/com/nbmlon/mushroomer/model/MushHistory.kt
@@ -1,11 +1,13 @@
 package com.nbmlon.mushroomer.model
 
 import org.joda.time.DateTime
+import java.io.Serializable
 import java.util.Date
 
 data class MushHistory (
+    val mushroom : Mushroom,
     val picPath : ArrayList<String>,
     val date : DateTime,
     val lat : Long,
     val lon : Long
-)
+) : Serializable
