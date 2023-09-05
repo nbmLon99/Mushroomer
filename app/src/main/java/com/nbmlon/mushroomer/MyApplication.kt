a69b657e27382bf7f3ea7599f292627b964767a0
nbmLon99 <rhfwleowkd77@gmail.com>
2023-09-06 06:36:01 +0900

    <Chore> Constants 따로 빼냄

--- app/src/main/java/com/nbmlon/mushroomer/MyApplication.kt
+++ app/src/main/java/com/nbmlon/mushroomer/MyApplication.kt
@@ -3,9 +3,5 @@ package com.nbmlon.mushroomer
 import android.app.Application
 
 class MyApplication : Application() {
-    class Constants {
-        companion object {
-            const val UNDEFINED = -1
-        }
-    }
+
 }
